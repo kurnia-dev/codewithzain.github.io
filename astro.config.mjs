7699520fbf1a4e2513911dcf150839eb65b0131e
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-12-24 22:17:27 +0700

    chore: update site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://codewithzain.github.io',
+	site: 'https://kurnia-dev.github.io',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });

c3c69fef956717c50a89d3204d6e6861bcf35203
Zain Kurnia <programmer-fe3@qwertysystem.net>
2026-01-16 01:59:37 +0700

    feat: Configure Astro to use the Node.js adapter for server-side rendering.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,58 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import { transformerMetaHighlight } from '@shikijs/transformers';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
+import { transformerMetaHighlight } from "@shikijs/transformers";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kurnia-dev.github.io',
-	image: {
-		// Enable image optimization
-		domains: ['lh3.googleusercontent.com'],
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'lh3.googleusercontent.com',
-			},
-		],
-	},
-	integrations: [
-		mdx(), 
-		sitemap(),
-		tailwind({
-			applyBaseStyles: false, // We'll handle base styles ourselves
-		})
-	],
-	build: {
-		inlineStylesheets: 'never'
-	},
-	markdown: {
-		shikiConfig: {
-			themes: {
-				light: 'catppuccin-mocha',
-				dark: 'catppuccin-mocha',
-			},
-			transformers: [
-				{
-					// Pass metadata and source code to the pre element
-					pre(hast) {
-						// Pass the raw metadata string
-						hast.properties['data-meta'] = this.options.meta?.__raw;
-						// Pass the original source code for copy functionality
-						hast.properties['data-code'] = this.source;
-					}
-				},
-				// Enable line highlighting with {1,3-5} syntax
-				transformerMetaHighlight(),
-			]
-		}
-	},
+  site: "https://kurnia-dev.github.io",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  image: {
+    // Enable image optimization
+    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false, // We'll handle base styles ourselves
+    }),
+  ],
+  build: {
+    inlineStylesheets: "never",
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-mocha",
+        dark: "catppuccin-mocha",
+      },
+      transformers: [
+        {
+          // Pass metadata and source code to the pre element
+          pre(hast) {
+            // Pass the raw metadata string
+            hast.properties["data-meta"] = this.options.meta?.__raw;
+            // Pass the original source code for copy functionality
+            hast.properties["data-code"] = this.source;
+          },
+        },
+        // Enable line highlighting with {1,3-5} syntax
+        transformerMetaHighlight(),
+      ],
+    },
+  },
 });
